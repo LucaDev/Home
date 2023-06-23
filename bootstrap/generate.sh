8a742d79ff5770e08b993dd9f9541a43e87ea201
Luca Kröger <luca.kroeger@pexon-consulting.de>
2023-06-23 13:21:00 +0200

    fix hwameistor

--- bootstrap/generate.sh
+++ bootstrap/generate.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sops --decrypt ./bootstrap/age-key.sops.yaml > age-key.yaml
-
 kubectl kustomize \
   --enable-helm \
   --load-restrictor=LoadRestrictionsNone \
@@ -12,4 +10,3 @@ yq -n '.cluster.inlineManifests[0].name="bootstrap" | .cluster.inlineManifests[0
 sed -i '' 's/\$/\$\$/g'  bootstrap.patch.yaml
 
 rm -rf cilium/charts
-rm age-key.yaml
